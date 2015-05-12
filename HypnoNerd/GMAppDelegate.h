584add4bfb67779d00d42793c7c990b79fd442c7
Ed McCormic <edmccormic@gmail.com>
2015-05-11 20:27:46 -0600

    Initial Commit

---
+++ HypnoNerd/GMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GMAppDelegate.h
+//  HypnoNerd
+//
+//  Created by ED on 5/11/15.
+//  Copyright (c) 2015 SwiftBeard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
